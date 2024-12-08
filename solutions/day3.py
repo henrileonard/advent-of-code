999ff24b476c231b9784dd0178c4d1f9ce67f640
henrileonard <henrihormel@gmail.com>
2024-12-08 19:58:19 +0100

    solve day 3 part 2

--- solutions/day3.py
+++ solutions/day3.py
@@ -1,11 +1,11 @@
 import re
 
+
 from utils.utils import read_string
 
 
 def part_one():
     calculation = read_string("day3.txt")
-    print(calculation)
     multiplications = re.findall("mul[(]\d+,\d+[)]", calculation)
     mul_sum = 0
     for entry in multiplications:
@@ -22,7 +22,23 @@ def part_one():
 
 
 def part_two():
-    print("implement me pls")
+    calculation = read_string("day3.txt")
+    multiplications = re.findall("mul[(]\d+,\d+[)]|do(?:n't)?[(][)]", calculation)
+    print(multiplications)
+    mul_sum = 0
+    enable = True
+    for entry in multiplications:
+        if entry == "don't()":
+            enable = False
+        elif entry == "do()":
+            enable = True
+        elif enable:
+            first, second = entry.split(",")
+            first = first[4:]
+            second = second[:-1]
+            mul_sum += int(first) * int(second)
+    print(mul_sum)
+
 
 
 
